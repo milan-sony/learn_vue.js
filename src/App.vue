4754168340d3abace078a843793d4ab16cfab39a
Milan Sony <milansonyofficial@gmail.com>
2024-03-18 14:15:42 +0530

    v-bind - multiple class binding

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,15 @@
 
   <div>{{ greetings }}</div>
   <div v-text="name"></div>
+
   <div v-bind:id="headingId">Heading</div>
   <button v-bind:disabled="isDisabled">Button</button>
+
   <h1 class="underline">Text Underline</h1>
   <h1 v-bind:class="status">Text Underline</h1>
   <h1 class="underline" v-bind:class="status">Text Underline</h1>
+  <h1 v-bind:class="isPromoted && 'promoted'">Promoted Movie</h1>
+  <h1 v-bind:class="isSoldout ? 'sold-out' : 'new'">Sold out movie</h1>
 
 </template>
 
@@ -14,13 +18,15 @@
 
 export default {
   name: "App",
-  data(){
-    return{
+  data() {
+    return {
       greetings: "Hello",
       name: "MilanSony",
       headingId: 'ID',
       isDisabled: true,
-      status: 'danger'
+      status: 'danger',
+      isPromoted: false,
+      isSoldout: true
     }
   }
 }
@@ -29,8 +35,20 @@ export default {
 
 <style>
 
-.underline{
+.underline {
   text-decoration: underline;
+}
+
+.promoted {
+  font-style: italic;
+}
+
+.new{
+  color: green;
+}
+
+.sold-out{
+  color: red;
 }
 
 </style>
