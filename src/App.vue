3575d5cdbdd22979bb7ed1211cb253e5d772107d
Milan Sony <milansonyofficial@gmail.com>
2024-03-18 19:54:08 +0530

    v-bind styleObj

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,10 @@
   <div>{{ greetings }}</div>
   <div v-text="name"></div>
 
+
   <div v-bind:id="headingId">Heading</div>
   <button v-bind:disabled="isDisabled">Button</button>
+
 
   <h1 class="underline">Text Underline</h1>
   <h1 v-bind:class="status">Text Underline</h1>
@@ -14,10 +16,21 @@
   <h1 v-bind:class="['new', 'promoted']">New Movie</h1>
   <h1 v-bind:class="[isPromoted && 'promoted', isSoldout ? 'sold-out' : 'new']">Array condition movie</h1>
   <h1 v-bind:class="{
-    promoted : isPromoted,
+    // key : class name, value : condition
+    promoted: isPromoted,
     new: !isSoldout,
-    'sold-out' : isSoldout
+    'sold-out': isSoldout
   }">Object condition movie</h1>
+
+
+  <h2 v-bind:style="{
+    // key : css property value : data property/condition
+    color: highlightColor,
+    'font-size': headerSize,
+    'padding': '20px'
+  }">Inline style</h2>
+
+  <h2 v-bind:style="headerStyleObj">Style Object</h2>
 
 </template>
 
@@ -33,7 +46,14 @@ export default {
       isDisabled: true,
       status: 'danger',
       isPromoted: true,
-      isSoldout: false
+      isSoldout: false,
+      highlightColor: 'orange',
+      headerSize: '50px',
+      headerStyleObj: {
+        color: 'Blue',
+        'font-size' : '30px',
+        padding: '10px'
+      }
     }
   }
 }
@@ -41,7 +61,6 @@ export default {
 </script>
 
 <style>
-
 .underline {
   text-decoration: underline;
 }
@@ -50,12 +69,11 @@ export default {
   font-style: italic;
 }
 
-.new{
+.new {
   color: green;
 }
 
-.sold-out{
+.sold-out {
   color: red;
 }
-
 </style>
