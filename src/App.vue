b9d279b01bf03bc25e1c3e377b65c00ca5eac703
Milan Sony <milansonyofficial@gmail.com>
2024-03-19 11:30:15 +0530

    methods and this keyword

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
 
   <h2>{{ 10 + 20 }}</h2>
 
-  <h2>Add method {{ add() }}</h2>
+  <h2>Add method {{ add(2, 3, 5) }}</h2>
+  <h2>Add method {{ add(2, 3, 2) }}</h2>
+  <h2>multiply {{ multiply(5) }}</h2>
 
 </template>
 
@@ -12,15 +14,17 @@ export default {
   name: "App",
   data() {
     return {
-
+      baseMultiplier:5
     }
   },
   methods:{
-    add(){
-      return 2+3+5
+    add(num1, num2, num3){
+      return num1 + num2 + num3
+    },
+    multiply(num){
+      return num * this.baseMultiplier
     }
   }
-
 }
 
 </script>
