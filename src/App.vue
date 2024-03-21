28ca902d8b1d718a173d453fc637316518567358
Milan Sony <milansonyofficial@gmail.com>
2024-03-21 12:13:09 +0530

    computed, diff b/w computed and method

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,11 @@
 <template>
 
-  <pre>
-    {{ JSON.stringify(formValue, null, 2) }}
-  </pre>
+  <h2>Fullname: {{ firstName }} {{ lastName }}</h2>
+  <h2>Computed fullname: {{ fullName }}</h2>
 
-  <form @submit="submitForm">
-
-    <div>
-      <p>Name</p>
-      <input type="text" id="name" v-model="formValue.name">
-    </div>
-
-    <div>
-      <p>Profile Summary</p>
-      <textarea id="profile" cols="30" rows="10" v-model="formValue.profileSummary"></textarea>
-    </div>
-
-    <div>
-      <p>Country</p>
-      <select id="country" v-model="formValue.country">
-        <option value="">Select a country</option>
-        <option value="india">India</option>
-        <option value="china">China</option>
-      </select>
-    </div>
-
-    <div>
-      <p>Job Location</p>
-      <select id="job-location" multiple v-model="formValue.jobLocation">
-        <option value="india">India</option>
-        <option value="china">China</option>
-      </select>
-    </div>
-
-    <div>
-      <input type="checkbox" id="remotework" v-model="formValue.remoteWork" true-value = 'yes' false-value = 'no'>
-      <span>Open to remote work?</span>
-    </div>
-
-    <div>
-      <p>Skills</p>
-      <input type="checkbox" id="html" value="html" v-model="formValue.skillset"><span>HTML</span>
-      <input type="checkbox" id="css" value="css" v-model="formValue.skillset"><span>CSS</span>
-      <input type="checkbox" id="javascript" value="javascript" v-model="formValue.skillset"><span>JavaScript</span>
-    </div>
-    <div>
-
-      <p>Years of exprerience</p>
-      <input type="radio" id="0-2" value="0-2" v-model="formValue.yearsOfExpreience"><span>0-2</span>
-      <input type="radio" id="3-5" value="3-5" v-model="formValue.yearsOfExpreience"><span>3-5</span>
-      <input type="radio" id="6-10" value="6-10" v-model="formValue.yearsOfExpreience"><span>6-10</span>
-    </div>
-
-    <div>
-      <button>SUMBIT</button>
-    </div>
-
-  </form>
+  <h2>Computed total: {{ total }}</h2>
+  <h2>Method Total: {{ getTotal() }}</h2>
+  <input type="text" v-model="country">
 
 </template>
 
@@ -66,21 +15,42 @@ export default {
   name: "App",
   data() {
     return {
-      formValue: {
-        name: '',
-        profileSummary: '',
-        country: '',
-        jobLocation: [],
-        remoteWork: 'no',
-        skillset: [],
-        yearsOfExpreience: ''
-      }
+      firstName: 'Milan',
+      lastName: 'Sony',
+      items: [
+        {
+          id: 1,
+          title: 'TV',
+          price: 100
+        },
+        {
+          id: 1,
+          title: 'Phone',
+          price: 200
+        },
+        {
+          id: 1,
+          title: 'Laptop',
+          price: 300
+        },
+      ],
+      country: ''
     }
   },
   methods: {
-    submitForm(event){
-      event.preventDefault()
-      console.log('Form Values', this.formValue)
+    getTotal(){
+      console.log('getTotal()')
+      return this.items.reduce((total, curr) => (total = total + curr.price), 0)
+    }
+
+  },
+  computed:{
+    fullName(){
+      return `${this.firstName} ${this.lastName}`
+    },
+    total(){
+      console.log('total()')
+      return this.items.reduce((total, curr) => (total = total + curr.price), 0)
     }
   }
 }
