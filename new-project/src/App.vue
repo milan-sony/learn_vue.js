b1368369dd26b66dc4dac03e461f0c9d9c22e1d6
Milan Sony <milansonyofficial@gmail.com>
2024-03-25 18:04:47 +0530

    initial commit, components

---
+++ new-project/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <GreetME />
+</template>
+
+<script>
+import GreetME from './components/Greet.vue'
+
+export default {
+  name: 'App',
+  components:{
+    GreetME
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ new-project/src/components/Greet.vue
@@ -0,0 +1,9 @@
+<template>
+    <h2>Hello Milan</h2>
+</template>
+
+<script>
+export default{
+    name: 'GreetMe'
+}
+</script>
