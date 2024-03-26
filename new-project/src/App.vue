8db2b7a8f558c657eb7a6edaa95f68f719d85a38
Milan Sony <milansonyofficial@gmail.com>
2024-03-26 17:50:55 +0530

    prop types (props as an object)

--- new-project/src/App.vue
+++ new-project/src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <GreetMe name="Milan" />
-  <GreetMe :name="name" :email="email" />
+  <!-- <GreetMe name="Milan" />
+  <GreetMe :name="name" :email="email" /> -->
+
+  <ArticleList title="Article" :likes="50" :isPublished="true" />
 </template>
 
 <script>
-import GreetMe from './components/Greet.vue'
+// import GreetMe from './components/Greet.vue'
+import ArticleList from './components/Article.vue'
 
 export default {
   name: 'App',
   components: {
-    GreetMe
+    // GreetMe,
+    ArticleList
   },
   data() {
     return {
